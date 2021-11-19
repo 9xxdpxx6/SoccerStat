d1884d99de2cd9dbb3c221f4779502f7914ac188
(004_25) Фролов Андрей <004_25@kep.local>
2021-11-19 09:43:11 +0300

    aboba

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ new Vue ({
 	el: '#app',
 	data: {
 		navbar_items: [
-			{title: 'Список лиг', href: ''},
+			{title: 'Список лиг', href: 'www.google.com'},
 			{title: 'Список команд', href: ''},
 			{title: 'Календарь лиги', href: ''},
 			{title: 'Календарь одной команды', href: ''}
