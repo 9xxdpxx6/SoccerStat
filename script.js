f0062705c361cc615c499603b9fcd80d9a8de2c0
(004_25) Фролов Андрей <004_25@kep.local>
2021-11-20 11:14:30 +0300

    new footbool api

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ new Vue ({
 	el: '#app',
 	data: {
 		navbar_items: [
-			{title: 'Список лиг', href: 'www.google.com'},
+			{title: 'Список лиг', href: ''},
 			{title: 'Список команд', href: ''},
 			{title: 'Календарь лиги', href: ''},
-			{title: 'Календарь одной команды', href: ''}
+			{title: 'Календарь одной команды', href: ''},
+			{title: '', href: ''}
+		],
+		leagues: [
+			{name: ''}
 		]
 	},
 	methods: {
@@ -15,3 +19,18 @@ new Vue ({
 
 	}
 });
+
+fetch("https://sportscore1.p.rapidapi.com/sports/1/teams?page=1", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "sportscore1.p.rapidapi.com",
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
