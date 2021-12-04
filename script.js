d36261ca644e637ffeffb4a213528744b45e265b
(004_25) Фролов Андрей <004_25@kep.local>
2021-12-04 12:49:17 +0300

    teams&leagues

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-new Vue ({
+let app = new Vue ({
 	el: '#app',
 	data: {
 		navbar_items: [
 			{title: 'Список лиг', href: ''},
 			{title: 'Список команд', href: ''},
-			{title: 'Календарь лиги', href: ''},
-			{title: 'Календарь одной команды', href: ''},
 			{title: '', href: ''}
 		],
-		leagues: [
-	
-		]
+		leagues: [],
+		teams: [],
 	},
 	methods: {
 
 	},
 	computed: {
 
-	}
+	}	
 });
 
+let teamNames = [];
+let teamIcons = [];
+
 $.ajax({
-  headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
-  url: 'https://api.football-data.org/v2/competitions',
-  dataType: 'json',
-  type: 'GET',
+  	headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
+  	url: 'https://api.football-data.org/v2/teams',
+ 	 dataType: 'json',
+ 	type: 'GET',
 }).done(function(response) {
-  
-  console.log(response);
+	for (let i = 0; i < response['teams'].length; i++) {
+		tName = teamNames.push(response['teams'][i]['name']),
+		tIcon = teamIcons.push(response['teams'][i]['crestUrl'])
+		let team = {
+			name: tName,
+			icon: tIcon
+		}
+		// app.teams.push(team);
+		console.log(tName);
+		// app.teams.push(response['teams'][i]['name'], response['teams'][i]['crestUrl']);
+	}
+	// console.log(teams);
+});
+
+
+
+$.ajax({
+  	headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
+  	url: 'https://api.football-data.org/v2/competitions',
+ 	 dataType: 'json',
+ 	type: 'GET',
+}).done(function(response) {
+	for (let i = 0; i < response['competitions'].length; i++)
+		app.leagues += response['competitions'][i]['name'];
 });
