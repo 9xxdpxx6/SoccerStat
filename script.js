41ca61248f85ea1a218dda7d705052d58dcb2701
9xxdpxx6 <89933562+9xxdpxx6@users.noreply.github.com>
2021-12-12 21:05:06 +0300

    Add files via upload
    123

--- script.js
+++ script.js
@@ -1,53 +1,59 @@
 let app = new Vue ({
 	el: '#app',
 	data: {
+		content: document.querySelector('.main'),
 		navbar_items: [
-			{title: 'Список лиг', href: ''},
-			{title: 'Список команд', href: ''},
-			{title: '', href: ''}
+			{title: 'Список лиг'},
+			{title: 'Список команд'}
+		],
+		titles: [
+			{text: 'Список лиг'},
+			{text: 'Список команд'},
+			{text: 'Календарь лиги ' + this.leagues},
+			{text: 'Календарь команды ' + this.teams},
 		],
 		leagues: [],
 		teams: [],
 	},
 	methods: {
-
+		goToNextPage: function(title) {
+			$.get('teams.html', function(data) {
+				if (title == 'Список лиг') {
+					this.content.innerHTML = data;
+				}
+			}, "text");
+		}
 	},
 	computed: {
 
-	}	
+	}
 });
 
-let teamNames = [];
-let teamIcons = [];
+$.ajax({
+	headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
+	url: 'https://api.football-data.org/v2/competitions',
+	dataType: 'json',
+	type: 'GET',
+}).done(function(response) {
+	for (let i = 0; i < response['competitions'].length; i++) {
+		let league = {
+			name: response['competitions'][i]['name'],
+		}
+		app.leagues.push(league);
+	}
+});
 
 $.ajax({
-  	headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
-  	url: 'https://api.football-data.org/v2/teams',
- 	 dataType: 'json',
+  headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
+  url: 'https://api.football-data.org/v2/teams',
+ 	dataType: 'json',
  	type: 'GET',
 }).done(function(response) {
 	for (let i = 0; i < response['teams'].length; i++) {
-		tName = teamNames.push(response['teams'][i]['name']),
-		tIcon = teamIcons.push(response['teams'][i]['crestUrl'])
 		let team = {
-			name: tName,
-			icon: tIcon
+			name: response['teams'][i]['name'],
+			icon: response['teams'][i]['crestUrl']
 		}
-		// app.teams.push(team);
-		console.log(tName);
-		// app.teams.push(response['teams'][i]['name'], response['teams'][i]['crestUrl']);
+		app.teams.push(team);
 	}
-	// console.log(teams);
-});
-
-
-
-$.ajax({
-  	headers: { 'X-Auth-Token': 'f3f3dad5142049a481c69a81fdda3d0d' },
-  	url: 'https://api.football-data.org/v2/competitions',
- 	 dataType: 'json',
- 	type: 'GET',
-}).done(function(response) {
-	for (let i = 0; i < response['competitions'].length; i++)
-		app.leagues += response['competitions'][i]['name'];
 });
